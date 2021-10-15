366988c4fc2a652342be3cf7ce35f0ad95f56cc7
bensuarez1 <G00292013@stu.raritanval.edu>
2021-10-14 22:31:10 -0400

    Added a Start Menu that loads into the game when the 'Start Game' button is pressed!

---
+++ Assets/__Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void OnMouseButton()
+    {
+        SceneManager.LoadScene("_Scene_0");
+    }
+}
